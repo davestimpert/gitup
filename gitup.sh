8a0b719c9ba2dc3295d7d5ee70f1b8ed00e1b69d
Dave Stimpert <dave@genomoncology.com>
2015-10-14 19:27:54 -0400

    initial commit - adding shell script

---
+++ gitup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+	
+RED='\033[33;31m'
+YELLO='\033[33;33m'
+GREEN='\033[33;32m'
+NC='\033[0m' # No Color
+
+HEAD=$(git rev-parse HEAD)
+CHANGED=$(git status --porcelain | wc -l)
+
+git fetch --all --prune
+for branch in `git for-each-ref --format='%(refname:short)' refs/heads`; do
+
+	LOCAL=$(git rev-parse --quiet --verify $branch)
+	if [ $HEAD == $LOCAL ] && [ $CHANGED -gt 0 ]; then
+		echo -e "${YELLO}WORKING${NC}\t\t$branch"
+	elif git rev-parse --verify --quiet $branch@{u}&>/dev/null; then
+		REMOTE=$(git rev-parse --quiet --verify $branch@{u})
+		BASE=$(git merge-base $branch $branch@{u})
+		
+		if [ $LOCAL == $REMOTE ]; then
+		   echo -e "${GREEN}OK${NC}\t\t$branch" 
+		elif [ $LOCAL == $BASE ]; then
+			git branch -f $branch $REMOTE
+			echo -e "${GREEN}UPDATED${NC}\t$branch"
+		elif [ $REMOTE == $BASE ]; then
+			echo -e "${RED}AHEAD${NC}\t\t$branch"
+		else
+			echo -e "${RED}DIVERGED${NC}\t$branch"
+		fi
+	else
+		echo -e "${RED}NO REMOTE${NC}\t$branch"
+	fi
+done
