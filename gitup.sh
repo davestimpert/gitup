369aa55937d868a1a01b976cc5b133b38d8b69e5
Dave Shah <dave.shah@genomoncology.com>
2015-10-14 20:47:32 -0400

    Use the non-synonym equals and function-fy

--- gitup.sh
+++ gitup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-	
+gitups(){	
 RED='\033[33;31m'
 YELLO='\033[33;33m'
 GREEN='\033[33;32m'
@@ -13,22 +13,22 @@ git fetch --all --prune &>/dev/null
 for branch in `git for-each-ref --format='%(refname:short)' refs/heads`; do
 
 	LOCAL=$(git rev-parse --quiet --verify $branch)
-	if [ $HEAD == $LOCAL ] && [ $CHANGED -gt 0 ]; then
+	if [ $HEAD = $LOCAL ] && [ $CHANGED -gt 0 ]; then
 		echo -e "${YELLO}WORKING${NC}\t\t$branch"
 	elif git rev-parse --verify --quiet $branch@{u}&>/dev/null; then
 		REMOTE=$(git rev-parse --quiet --verify $branch@{u})
 		BASE=$(git merge-base $branch $branch@{u})
 		
-		if [ $LOCAL == $REMOTE ]; then
+		if [ $LOCAL = $REMOTE ]; then
 		   echo -e "${GREEN}OK${NC}\t\t$branch" 
-		elif [ $LOCAL == $BASE ]; then
-			if [ $HEAD == $LOCAL ]; then
+		elif [ $LOCAL = $BASE ]; then
+			if [ $HEAD = $LOCAL ]; then
 				git merge $REMOTE&>/dev/null
 			else
 				git branch -f $branch $REMOTE
 			fi
 			echo -e "${GREEN}UPDATED${NC}\t\t$branch"
-		elif [ $REMOTE == $BASE ]; then
+		elif [ $REMOTE = $BASE ]; then
 			echo -e "${RED}AHEAD${NC}\t\t$branch"
 		else
 			echo -e "${RED}DIVERGED${NC}\t\t$branch"
@@ -37,3 +37,4 @@ for branch in `git for-each-ref --format='%(refname:short)' refs/heads`; do
 		echo -e "${RED}NO REMOTE${NC}\t$branch"
 	fi
 done
+}
